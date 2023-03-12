8405267f06e261d4a4ea8584f4f4fa026bbc605c
Karla Gissell Benítez Quintanilla <124007184+Gissell-Quintanilla@users.noreply.github.com>
2023-03-11 20:20:09 -0600

    Asíncrona

---
+++ ActividadAsíncrona_11-03-23/ActividadAsíncrona_11-03-23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ingrese un número: ");
+int Triángulo = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= Triángulo; i++)
+{
+    String temp = "";
+    for (int t = 1; t <= i; t++) 
+    {
+        temp += "*";
+    }
+    Console.WriteLine(temp);
+}   
